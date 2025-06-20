8aca4d0c9f2998d4e53e77d2e2e5ad12723b7c41
Parth Gohel <gohelparth444@gmail.com>
2025-06-20 17:13:59 +0530

    Update Register.php

--- Register.php
+++ Register.php
@@ -3,190 +3,114 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <title>Ragistration Page</title>
+    <title>Registration Page</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
     <style>
-
-        body
-        {
-            margin: 0px;
-            padding: 0px;
-            background-color: rgb(54, 52, 52);
+        body {
+            background-color: #363434;
         }
-
-
-        .container-1
-        {
-            display: flex;
-        }
-
-        .box-1
-        {
-            margin : 20px 0px 30px 180px;
-            width: 380px;
-            height: 510px;
-            background-color: black;
+        .form-icon {
             position: absolute;
-            border-radius: 5px;
-            border-bottom-right-radius: 0px;
-            border-top-right-radius: 0px;
-        }
-
-        .box-2
-        {
-            width: 550px;
-            height: 510px;
-            background-color: white;
-            margin: 20px 0px 0px 560px;
-            position: absolute;
-            border-radius: 5px;
-            border-bottom-left-radius: 0px;
-            border-top-left-radius: 0px;
-        }
-        
-        .title
-        {
-            font-size: xx-large;
-            color: black;
-            text-align: center;
-            margin: 0px;
-        }
-
-        .feild
-        {
-            width: 300px;
-            height: 25px;
-            outline: none;
-            border: 0px;
-            color: black;
-            background-color: rgb(232, 231, 231);
-            border-radius: 3px;
-            margin-top: 0px;
-            margin-bottom: 2px;
-            font-size: medium;
-            padding: 10px;
-            border-bottom-left-radius: 0px;
-            border-top-left-radius: 0px;
-        }
-        
-        .feild::placeholder
-        {
-            color: black;
             left: 10px;
-            position: absolute;
-            display: none;
+            top: 50%;
+            transform: translateY(-50%);
+            color: #6c757d;
         }
-
-        .register-button
-        {
-            width: 220px;
-            height: 40px;
-            margin-top: 30px;
-            color: white;
+        .input-group-text {
+            background-color: #e8e7e7;
+            border: none;
+        }
+        .form-control {
+            background-color: #e8e7e7;
+            border: none;
+            padding-left: 40px;
+        }
+        .form-section {
+            height: 100vh;
+        }
+        .left-box {
             background-color: black;
-            font-size: large;
-            border-radius: 25px;
-            border: 2px solid rgb(36, 7, 102);
-        }
-
-        .register-button:hover
-        {
-            width: 240px;
-            height: 40px;
-            margin-top: 30px;
             color: white;
-            background-color: rgb(2, 2, 2);
-            font-size: large;
-            border-radius: 25px;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            padding: 2rem;
+            border-top-left-radius: 10px;
+            border-bottom-left-radius: 10px;
         }
-
-        #icon
-        {
-            margin-right: 0px;
-            font-size: 21px;
-            background-color:rgb(232, 231, 231) ;
-            width: 40px;
-            height: 35px;
-            border-radius: 3px;
-            padding-top: 10px;
-            margin-top: 10px;
-            border-top-right-radius: 0px;
-            border-bottom-right-radius: 0px;
+        .right-box {
+            background-color: white;
+            padding: 2rem;
+            border-top-right-radius: 10px;
+            border-bottom-right-radius: 10px;
         }
-
-        .title-2
-        {
-            margin-top: 130px;
-            color: white;
-            padding-left: 50px;
-            height: 250px;
-            width: 250px;
-            text-align: center; 
-        }
-
-         .login-button
-        {
-            width: 220px;
-            height: 40px;
-            margin-top: 30px;
-            color: white;
+        .btn-custom {
             background-color: black;
-            font-size: large;
-            border-radius: 25px;
+            color: white;
+            border: 2px solid rgb(25, 152, 139);
+        }
+        .btn-custom:hover {
+            background-color: #000000;
+            color:white;
+        }
+        .btn-login {
             border: 2px solid white;
+            background-color: black;
+            color:white;
         }
-
-
-        .login-button:hover
-        {
-            width: 240px;
-            height: 40px;
-            margin-top: 30px;
-            color: white;
-            background-color: rgb(2, 2, 2);
-            font-size: large;
-            border-radius: 25px;
+        .btn-login:hover {
+            background-color: #000000;
+            color:white;
         }
-
     </style>
-
-    <script>
-         function myFunction() {
-            
-    }
-    </script>
 </head>
 <body>
-    <div class="container-1">
-        <div class="box-1">  
-            <div class="title-2">
-                <h1>Welcome Back!</h1>
-                <h5>To keep connected with us please login with your personal info </h5>
-                <button class="login-button" onclick="location.href = 'Login.php';" id="myButton" class="float-left submit-button" >Sign in</button>
-            </div>
+
+<div class="container d-flex align-items-center justify-content-center form-section">
+    <div class="row w-100 shadow rounded" style="max-width: 900px;">
+        <div class="col-md-5 left-box text-center">
+            <h1>Welcome Back!</h1>
+            <p class="mb-5">To keep connected with us please login with your personal info</p>
+            <a href="Login.php" class="btn btn-login rounded-pill px-4">Sign in</a>
         </div>
-        <div class="box-2">
-            <div class="title">
-                <h5>Create Account</h5>
-                <form action="Register.php" method="post">
-                    <i id="icon" class="fa-solid fa-circle-user"></i><input type="text" placeholder="Username" name="txtunm" required class="feild"><br>
-                    <i id="icon" class="fa-solid fa-envelope"></i><input type="email" placeholder="E-mail" name="txteml" required class="feild"><br>
-                    <i id="icon" class="fa-solid fa-phone"></i><input type="number" placeholder="Contect No." name="txtcont" required class="feild"><br>
-                    <i id="icon" class="fa-solid fa-location-dot"></i><input type="text" placeholder="Address" name="txtadr" required class="feild"><br>
-                    <i id="icon" class="fa-solid fa-lock"></i><input type="password" placeholder="Password" name="txtpwd" required class="feild"><br>
-                    <input type="submit"  value="Sign Up" class="register-button">
-                </form>
-            </div> 
+        <div class="col-md-7 right-box">
+            <h3 class="text-center mb-4">Create Account</h3>
+            <form action="Register.php" method="post">
+                <div class="mb-3 position-relative">
+                    <i class="fa fa-user form-icon"></i>
+                    <input type="text" class="form-control ps-5" name="txtunm" placeholder="Username" required>
+                </div>
+                <div class="mb-3 position-relative">
+                    <i class="fa fa-envelope form-icon"></i>
+                    <input type="email" class="form-control ps-5" name="txteml" placeholder="E-mail" required>
+                </div>
+                <div class="mb-3 position-relative">
+                    <i class="fa fa-phone form-icon"></i>
+                    <input type="text" class="form-control ps-5" name="txtcont" placeholder="Contact No." required>
+                </div>
+                <div class="mb-3 position-relative">
+                    <i class="fa fa-location-dot form-icon"></i>
+                    <input type="text" class="form-control ps-5" name="txtadr" placeholder="Address" required>
+                </div>
+                <div class="mb-3 position-relative">
+                    <i class="fa fa-lock form-icon"></i>
+                    <input type="password" class="form-control ps-5" name="txtpwd" placeholder="Password" required>
+                </div>
+                <div class="text-center">
+                    <button type="submit" class="btn btn-custom rounded-pill px-5 mt-3">Sign Up</button>
+                </div>
+            </form>
         </div>
     </div>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
 
-<?php
-    ob_start();
-    ob_flush();
 
-    $con = mysqli_connect("localhost","root","","SweetDish");
+<?php
+    $con = mysqli_connect("localhost","root","","Fastfood");
     if(isset($_POST['txtunm']))
     {  
         $unm = $_POST['txtunm'];
@@ -206,3 +130,5 @@
         }
     }
 ?>
+
+
